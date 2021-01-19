879409282898e27a322392d07766c279aa3a5861
eminedari <43938811+eminedari@users.noreply.github.com>
2021-01-19 21:02:38 +0300

    fixed file name

--- db_init.py
+++ db_init.py
@@ -98,6 +98,6 @@ def initialize(url):
 if __name__ == "__main__":
     url = os.getenv("DATABASE_URL")
     if url is None:
-        print("Usage: DATABASE_URL=url python dbinit.py")  # , file=sys.stderr)
+        print("Usage: DATABASE_URL=url python db_init.py")  # , file=sys.stderr)
         sys.exit(1)
     initialize(url)
